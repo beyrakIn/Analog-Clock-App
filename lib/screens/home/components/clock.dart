92d7c9e414f31933e350a0708c74866367b33d52
B E Y R ∆ K <68893229+beyrakIn@users.noreply.github.com>
2021-02-20 20:49:45 +0400

    Clock

--- lib/screens/home/components/clock.dart
+++ lib/screens/home/components/clock.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 import 'clock_painter.dart';
-
 
 class Clock extends StatefulWidget {
   @override
@@ -11,6 +14,16 @@ class Clock extends StatefulWidget {
 
 class _ClockState extends State<Clock> {
   DateTime _dateTime = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _dateTime = DateTime.now();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +40,16 @@ class _ClockState extends State<Clock> {
                     offset: Offset(0, 0),
                     color: kShadowColor.withOpacity(0.14),
                     blurRadius: 64)
-              ]),
-          child: CustomPaint(
-            painter: ClockPainter(context, _dateTime),
+              ]
+          ),
+          child: Transform.rotate(
+            angle: -pi / 2,
+            child: CustomPaint(
+              painter: ClockPainter(context, _dateTime),
+            ),
           ),
         ),
       ),
     );
   }
 }
-
