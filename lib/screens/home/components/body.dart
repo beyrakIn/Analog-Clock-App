c05eeb02919b60c06fa213c35e419a15a2aea271
B E Y R ∆ K <68893229+beyrakIn@users.noreply.github.com>
2021-02-20 20:27:43 +0400

    Clock

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:clock/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:clock/screens/home/components/time_in_hour_and_minute.dart';
 
@@ -13,8 +14,88 @@ class Body extends StatelessWidget {
             style: Theme.of(context).textTheme.bodyText1,
           ),
           TimeInHourAndMinute(),
+          Clock()
         ],
       ),
     );
+  }
+}
+
+class Clock extends StatefulWidget {
+  @override
+  _ClockState createState() => _ClockState();
+}
+
+class _ClockState extends State<Clock> {
+  DateTime _dateTime = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(0, 0),
+                    color: kShadowColor.withOpacity(0.14),
+                    blurRadius: 64)
+              ]),
+          child: CustomPaint(
+            painter: ClockPainter(context, _dateTime),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  final BuildContext context;
+  final DateTime dateTime;
+  ClockPainter(this.context, this.dateTime);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double centerX = size.width / 2;
+    double centerY = size.height / 2;
+    Offset center = Offset(centerX, centerY);
+
+    // Minute Line
+    canvas.drawLine(
+        center,
+        Offset(40, 180),
+        Paint()
+          ..color = Theme.of(context).accentColor
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 10);
+
+    // Hour Line
+    canvas.drawLine(
+        center,
+        Offset(90, 200),
+        Paint()
+          ..color = Theme.of(context).primaryColor
+          ..strokeWidth = 10);
+
+    // Second Line
+    canvas.drawLine(center, Offset(80, 150),
+        Paint()..color = Theme.of(context).primaryColor);
+
+    //  Center Dots
+    Paint dotPainter = Paint()
+      ..color = Theme.of(context).primaryIconTheme.color;
+    canvas.drawCircle(center, 24, dotPainter);
+    canvas.drawCircle(
+        center, 23, Paint()..color = Theme.of(context).backgroundColor);
+    canvas.drawCircle(center, 10, dotPainter);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw true;
   }
 }
