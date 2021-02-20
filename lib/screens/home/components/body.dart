0cbad0528f83acf77749f488b043cc1c35e765e4
B E Y R ∆ K <68893229+beyrakIn@users.noreply.github.com>
2021-02-20 20:33:39 +0400

    Clock

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:clock/constants.dart';
-import 'package:flutter/material.dart';
 import 'package:clock/screens/home/components/time_in_hour_and_minute.dart';
+import 'package:flutter/material.dart';
+
+import 'clock_painter.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -28,6 +30,7 @@ class Clock extends StatefulWidget {
 
 class _ClockState extends State<Clock> {
   DateTime _dateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -53,49 +56,3 @@ class _ClockState extends State<Clock> {
   }
 }
 
-class ClockPainter extends CustomPainter {
-  final BuildContext context;
-  final DateTime dateTime;
-  ClockPainter(this.context, this.dateTime);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    double centerX = size.width / 2;
-    double centerY = size.height / 2;
-    Offset center = Offset(centerX, centerY);
-
-    // Minute Line
-    canvas.drawLine(
-        center,
-        Offset(40, 180),
-        Paint()
-          ..color = Theme.of(context).accentColor
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = 10);
-
-    // Hour Line
-    canvas.drawLine(
-        center,
-        Offset(90, 200),
-        Paint()
-          ..color = Theme.of(context).primaryColor
-          ..strokeWidth = 10);
-
-    // Second Line
-    canvas.drawLine(center, Offset(80, 150),
-        Paint()..color = Theme.of(context).primaryColor);
-
-    //  Center Dots
-    Paint dotPainter = Paint()
-      ..color = Theme.of(context).primaryIconTheme.color;
-    canvas.drawCircle(center, 24, dotPainter);
-    canvas.drawCircle(
-        center, 23, Paint()..color = Theme.of(context).backgroundColor);
-    canvas.drawCircle(center, 10, dotPainter);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    throw true;
-  }
-}
